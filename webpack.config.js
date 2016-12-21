256e691d15edbd326f2ec70ff66146823daee2da
vaenow <luowenbiz@gmail.com>
2016-12-21 23:47:47 +0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+
+var path = require("path")
+var webpack = require("webpack")
+
+module.exports = {
+    entry: {
+        index: path.join(__dirname, "www", "js", "index"),
+    },
+    output: {
+        path: path.join(__dirname, "www", "js-dest"), 
+        filename: "[name].js",
+    },
+    loaders: [
+        {
+            test:/.js$/, 
+            exclude: /node_modules/,
+            loader: "babel",
+            query: {
+                preset: ['es2015']
+            }
+        },
+        {test: /\.css$/,loader:"css"}
+    ],
+    plugins:[
+        new webpack.optimize.UglifyJsPlugin(),
+    ]
+    
+}
+
+
